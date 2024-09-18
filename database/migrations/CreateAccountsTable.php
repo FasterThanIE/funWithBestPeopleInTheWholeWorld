afc17d0a88327c6a2a0dbef57600ea77a6be7b33
PC <dijanic@gmail.com>
2024-09-18 22:55:10 +0200

    * Create accounts and model for accounts

---
+++ database/migrations/CreateAccountsTable.php
@@ -0,0 +1,14 @@
+<?php
+
+use App\Emrah\Models\Account;
+use App\Emrah\Models\User;
+use Illuminate\Database\Schema\Blueprint;
+
+$schema->create(table: Account::TABLE, callback: function(Blueprint $table) {
+    $table->id();
+    $table->bigInteger(column: 'amount');
+    $table->string(column: 'status', length: 15);
+
+    $table->foreignId(column: 'owner')->constrained(table: User::TABLE);
+    $table->timestamps();
+});
---
+++ src/Models/Account.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace App\Emrah\Models;
+
+class Account
+{
+    const TABLE = "accounts";
+}
