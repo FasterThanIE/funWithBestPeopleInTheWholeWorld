6c80c76a874ef7324298d7ed08a47b59dd7e2a2f
PC <dijanic@gmail.com>
2024-09-20 21:29:48 +0200

    * accounts migration

--- database/migrations/CreateAccountsTable.php
+++ database/migrations/CreateAccountsTable.php
@@ -8,7 +8,6 @@ $schema->create(table: Account::TABLE, callback: function(Blueprint $table) {
     $table->id();
     $table->bigInteger(column: 'amount');
     $table->string(column: 'status', length: 15);
-
     $table->foreignId(column: 'owner')->constrained(table: User::TABLE);
     $table->timestamps();
 });
