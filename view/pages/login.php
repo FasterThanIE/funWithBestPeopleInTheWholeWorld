f0f4e00762a9cbb1c7ad450afe3a2f4719cd93fe
PC <dijanic@gmail.com>
2024-09-20 22:17:32 +0200

    * Sexy login page

---
+++ view/pages/login.php
@@ -0,0 +1,18 @@
+<form class="formCentered">
+
+    <h1>Login</h1>
+    <h5>Bole</h5>
+
+    <div class="inputHolder">
+        <input id="email" type="email" required />
+        <label for="email">Unesite vasu email adresu</label>
+    </div>
+
+    <div class="inputHolder">
+        <input id="password" type="password" required />
+        <label for="password">Unesite vasu lozinku</label>
+    </div>
+
+    <button class="btn mt-1">Login</button>
+
+</form>
---
+++ view/template.php
@@ -0,0 +1,11 @@
+<html lang="en">
+
+    <head>
+        <link rel="stylesheet" type="text/css" href="../public/css/style.css">
+    </head>
+
+    <body>
+        <?php require_once "pages/login.php"; ?>
+    </body>
+
+</html>
