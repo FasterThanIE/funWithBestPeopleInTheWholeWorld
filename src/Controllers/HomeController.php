97031c748afeabd1d74cc5944f9cb9766cd00f2c
PC <dijanic@gmail.com>
2024-09-20 22:49:50 +0200

    * Fix return type

--- src/Controllers/HomeController.php
+++ src/Controllers/HomeController.php
@@ -4,7 +4,7 @@ namespace App\Emrah\Controllers;
 
 class HomeController
 {
-    public function index()
+    public function index(): array
     {
         return [
             'view' => 'homepage'
