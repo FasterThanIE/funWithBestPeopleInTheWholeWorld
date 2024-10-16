93ba78e06af4e6a53f02c7da0fb362a3f799e3bd
PC <dijanic@gmail.com>
2024-10-16 22:25:56 +0200

    * Logic to showcase routes

--- index.php
+++ index.php
@@ -13,6 +13,15 @@ $route = $gubkaRoutes[$userRoute]; // $gubkaRoutes['/home'] => [ 'controller'   
 
 $controller = new $route['controller']; // HomeController -> new HomeController;
 
-$response = $controller->{$route['function']}(); // pozovi index funkciju
 
-require_once "view/template.php";
+// TODO: Refactor to use OOP, maybe builder or factory or wtf something?
+// Problem: Some routes require paramters passed to functions, some dont
+// Some require redirects, some dont
+// Sug: Builder approach
+if(isset($route['functiona']) && $route['functional']) {
+    $controller->{$route['function']}($_REQUEST);
+} else {
+    $response = $controller->{$route['function']}();
+    require_once "view/template.php";
+}
+
